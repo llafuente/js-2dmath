ba1d99f55def474967aefbdbc37152fc589c1121
llafuente <llafuente@noboxout.com>
2013-10-15 16:34:18 +0200

    2d transformation matrix

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ require("./lib/math.js");
 module.exports = {
     Vec2: require("./lib/vec2.js"),
     Rectangle: require("./lib/rectangle.js"),
-    Circle: require("./lib/circle.js")
+    Circle: require("./lib/circle.js"),
+    Matrix2D: require("./lib/matrix2d.js")
 };
---
+++ test/matrix2d.js
@@ -0,0 +1,28 @@
+require("../lib/math.js");
+
+var tap = require("tap"),
+    test = tap.test,
+    Vec2 = require("../lib/vec2.js"),
+    Matrix2D = require("../lib/matrix2d.js");
+
+//setup
+
+test("Vec2 init", function(t) {
+    var m2d1 = Matrix2D(),
+        m2d2 = Matrix2D(),
+        m2d3 = Matrix2D();
+
+    Matrix2D.identity(m2d2);
+
+    t.deepEqual(m2d1, m2d2, "t1");
+
+
+    Matrix2D.translate(m2d2, m2d2, [10, 10]);
+
+    t.deepEqual(m2d2, [1, 0, 0, 1, 20, 11, [1, 1, 0, 0, 0], true], "t1");
+
+    Matrix2D.copy(m2d1, m2d2);
+    t.deepEqual(m2d1, m2d2, "t1");
+
+    t.end();
+});
