2d11f85b98d1df57723318c5d71f616dc1110736
llafuente <llafuente@noboxout.com>
2013-10-15 16:54:10 +0200

    Segment2

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
     Vec2: require("./lib/vec2.js"),
     Rectangle: require("./lib/rectangle.js"),
     Circle: require("./lib/circle.js"),
+    Segment2: require("./lib/segment2.js"),
     Matrix2D: require("./lib/matrix2d.js")
 };
